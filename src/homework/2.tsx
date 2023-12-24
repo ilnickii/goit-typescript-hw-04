64e41cdb82d4165943281788aa1e7772000832fb
ilnickii <ilnickii2008@gmail.com>
2023-12-24 18:58:35 +0300

    added hw ts 04

--- src/homework/2.tsx
+++ src/homework/2.tsx
@@ -1,5 +1,18 @@
 import React, {useReducer} from "react";
 
+interface State {
+  requestStep: "start" | "pending" | "finished" | "idle";
+  isRequestInProgress: boolean;
+}
+
+interface Action {
+  type:
+    | "START_REQUEST"
+    | "PENDING_REQUEST"
+    | "FINISH_REQUEST"
+    | "RESET_REQUEST";
+}
+
 const initialState: State = {
   isRequestInProgress: false,
   requestStep: 'idle',
